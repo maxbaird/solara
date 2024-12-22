fa29d51922bada51d9cad01dc61f299b71192b40
Max M Baird <maxbaird.gy@gmail.com>
2024-12-22 21:18:29 +0100

    Added comments.

--- lib/core/resources/solara_io_exception.dart
+++ lib/core/resources/solara_io_exception.dart
@@ -1,13 +1,26 @@
 import 'package:http/http.dart';
 
+/// The exception type for IO operations.
 enum IOExceptionType {
+  /// If the IO operation times out.
   connectTimeout,
+
+  /// If the server request fails.
   server,
+
+  /// If there was an error with local storage IO.
   localStorage,
+
+  /// Any other error.
   other,
+
+  /// Error performing a conversion.
   conversion,
 }
 
+/// IO exceptions thrown by the application when interacting with an IO device.
+///
+/// IO devices are typically local storage or over the internet.
 class SolaraIOException implements Exception {
   SolaraIOException({
     this.response,
@@ -16,14 +29,22 @@ class SolaraIOException implements Exception {
     this.type = IOExceptionType.other,
   });
 
+  /// The type of IOException.
   IOExceptionType type;
 
+  /// The response (typically an http response).
   Response? response;
 
+  /// The error that occurred.
   dynamic error;
+
+  /// An error map to parse any error messages from the server.
   Map<String, String>? errorMap;
+
+  /// The stacktrace of the exception.
   StackTrace? stackTrace;
 
+  /// A string representation of [error].
   String get message => error?.toString() ?? '';
 
   @override
