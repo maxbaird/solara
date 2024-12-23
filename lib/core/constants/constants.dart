11f2096b9f95de8973a8f1c595834289dbf9e48d
Max M Baird <maxbaird.gy@gmail.com>
2024-12-23 02:35:26 +0100

    Verified application runs on ios.

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,2 +1,5 @@
+import 'dart:io' show Platform;
+
 /// The Url of the remote connection hosting the endpoints.
-const baseUrl = 'http://10.0.2.2:3000';
+String get baseUrl =>
+    Platform.isAndroid ? 'http://10.0.2.2:3000' : 'http://localhost:3000';
