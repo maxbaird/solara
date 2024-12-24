0389328c7d40f6fc5238b17c733b06d44c3538c0
Max M Baird <maxbaird.gy@gmail.com>
2024-12-24 21:53:08 +0100

    Added comments.

--- lib/core/presentation/model/ui_model.dart
+++ lib/core/presentation/model/ui_model.dart
@@ -3,7 +3,12 @@ import '../util/flows/solara_plot_data.dart';
 
 /// Contains the fields that all UiModels for Solara must expose.
 abstract interface class UiModel {
+  /// The date of [plotData] being used for the chart.
   DateTime get date;
+
+  /// The type of units to use when labelling the y-axis of the chart.
   SolaraUnitType get unitType;
+
+  /// The data used to plot the chart.
   SolaraPlotData get plotData;
 }
