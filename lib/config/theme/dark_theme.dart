7aa639853cb16eae8facf2146d2e36771d7f52b1
Max M Baird <maxbaird.gy@gmail.com>
2024-12-25 06:05:38 +0100

    Mostly done with dark theme.

--- lib/config/theme/dark_theme.dart
+++ lib/config/theme/dark_theme.dart
@@ -16,4 +16,42 @@ final ThemeData themeDataDark = ThemeData.dark(
   useMaterial3: true,
 ).copyWith(
   colorScheme: _colorSchemeDark,
+  appBarTheme: AppBarTheme().copyWith(
+    backgroundColor: kSolaraDarkGreen,
+    titleTextStyle: TextStyle(color: kSolaraWhite),
+  ),
+  tabBarTheme: TabBarTheme().copyWith(
+    indicatorColor: kSolaraGreenDark2,
+    dividerColor: kSolaraGreyLight,
+    labelColor: kSolaraGreenDark2,
+    unselectedLabelColor: kSolaraGreyLight,
+  ),
+  scaffoldBackgroundColor: kSolaraDarkGreen,
+  switchTheme: SwitchThemeData().copyWith(
+    trackColor: WidgetStateColor.resolveWith(
+      (states) {
+        if (states.contains(WidgetState.selected)) {
+          return kSolaraGreenDark;
+        } else {
+          return kSolaraGreyGreen;
+        }
+      },
+    ),
+    thumbColor: WidgetStateColor.resolveWith(
+      (states) {
+        if (states.contains(WidgetState.selected)) {
+          return kSolaraWhite;
+        } else {
+          return kSolaraGreenDark;
+        }
+      },
+    ),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      foregroundColor: kSolaraGreenDark,
+      iconColor: kSolaraGreenDark,
+      backgroundColor: kSolaraGreyGreen,
+    ),
+  ),
 );
